cdfed3af593b5b130ef2fb60238e30146cd36ba8
deathfated <60390126+deathfated@users.noreply.github.com>
2022-06-08 20:13:43 +0700

    Base Gameplay
    Pong base game; Ball, Walls and Paddles now in Rigidbody and can Bounce

---
+++ Assets/PaddyController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddyController : MonoBehaviour
+{
+    public int speed;
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //move object & //get input
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+
+        if (Input.GetKey(upKey))
+        {
+            return Vector2.up * speed;
+        }
+        if (Input.GetKey(downKey))
+        {
+            return Vector2.down * speed;
+        }
+
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        rig.velocity = movement;
+    }
+}
